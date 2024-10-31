45d9c93bfe28a52f438002831aa68b0cf3736e01
Itzel <Itzel@Itzan23.dhcp.wustl.edu>
2024-10-31 12:47:52 -0500

    Today

---
+++ src/studio7/Rectangle.java
@@ -0,0 +1,13 @@
+package studio7;
+
+public class Rectangle {
+	private double length;
+	private double width;
+
+	
+	public Rectangle(double inLength, double inWidth) {
+		length = inWidth;
+		widthi
+	}
+
+}
